2d41e3ec614174521d66e0205e40573c3a53e9b6
polporta <PP191229@falmouth.ac.uk>
2020-08-18 16:17:51 +0100

    dsdas

--- DDA2/Assets/Scripts/BoundaryManager.cs
+++ DDA2/Assets/Scripts/BoundaryManager.cs
@@ -17,7 +17,7 @@ public class BoundaryManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ManageBoundary();
     }
 
     void ManageBoundary()
